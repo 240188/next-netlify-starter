39082ba42f25716135f1e8100e57a9b4717eabf8
240188 <42891200+240188@users.noreply.github.com>
2022-01-19 12:53:45 +0100

    først endrig

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Velkommen til Kaldestad traffikk" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Her vil det komme mer info etter kvart<code>...</code>
         </p>
       </main>
 
